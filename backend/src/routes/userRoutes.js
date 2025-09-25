4ef1dd37c4101f2bc871ac41913b759e072e4d9f
tarequr <tarequrr8@gmail.com>
2025-09-25 23:54:09 +0600

    chore: show user

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userController');
 
 router.get('/', userController.getUsers);
 router.post('/', userController.createUser);
+router.get('/show', userController.createUser);
 
 module.exports = router;
